4a383b5613da68fc2bab9e9fe4d33fa931aa1af2
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-02-11 12:55:15 +0300

    Modularity in the config.webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,43 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const pages = require('./webpack/pages');
+const pug = require('./webpack/pug');
+const devserver = require('./webpack/devserver');
 
 const PATHS = {
   source: path.join(__dirname, 'source'),
-  build: path.join(__dirname, 'build'),
-  pages: path.join(__dirname, 'source/pages')
+  build: path.join(__dirname, 'build')
 };
 
-const common = {
-  entry: {
-    index: PATHS.pages + '/index/index.js',
-    portfolio: PATHS.pages + '/portfolio/index.js',
-    blog: PATHS.pages + '/blog/index.js',
-    about: PATHS.pages + '/about/index.js'
+const common = merge([
+  {
+    entry: {},
+    output: {
+      path: PATHS.build,
+      filename: './js/[name].js'
+    },
   },
-  output: {
-    path: PATHS.build,
-    filename: './js/[name].js'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      chunks: ['index'],
-      template: PATHS.pages + '/index/index.pug',
-      filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['portfolio'],
-      template: PATHS.pages + '/portfolio/index.pug',
-      filename: 'portfolio.html'
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['blog'],
-      template: PATHS.pages + '/blog/index.pug',
-      filename: 'blog.html'
-    }),
-    new HtmlWebpackPlugin({
-      chunks: ['about'],
-      template: PATHS.pages + '/about/index.pug',
-      filename: 'about.html'
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.pug$/,
-        loader: 'pug-loader', 
-        options: {
-          pretty: true
-        }
-      }
+  pages({ 
+    pathTo: `${PATHS.source}/pages`,
+    aliases: [
+      'index', 
+      'about',
+      'blog', 
+      'portfolio', 
     ]
-  }
-};
-
-const developmentConfig  = {
-    devServer: {
-        stats: 'errors-only'
-    }
-};
+  }),
+  pug()
+]);
 
 
-module.exports = function(env) {
+module.exports = (env) => {
     if (env === 'production') {
         return common;
     }
     if (env === 'development') {
-        return Object.assign(
-            {},
+        return merge([
             common,
-            developmentConfig
-        );
+            devserver()
+        ]);
     }
 };
---
+++ webpack/pages.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+function getEntries(pathTo, aliases) {
+    let r = {};
+    aliases.forEach((alias) => r[alias] = `${pathTo}/${alias}/${alias}.js`);
+    return r;
+}
+
+function getPlugins(pathTo, aliases) {
+    return aliases.map((alias) => {
+        return getPlugin(pathTo, alias);
+    })
+}
+
+function getPlugin(pathTo, alias) {
+    return new HtmlWebpackPlugin({
+        chunks: [alias],
+        template: `${pathTo}/${alias}/index.pug`,
+        filename: `${alias}.html`
+    })
+}
+
+module.exports = function({pathTo, aliases}) {
+    return {
+        entry: getEntries(pathTo, aliases),
+        plugins: getPlugins(pathTo, aliases)
+    };
+};
