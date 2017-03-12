09fdf4a73b41b13bae969e6f7b2204d5757bd846
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-03-12 13:16:25 +0300

    Refactoring module 'menu'

--- source/js/modules/menu.js
+++ source/js/modules/menu.js
@@ -8,26 +8,28 @@ module.exports = (() => {
         _active = false,
         _$page = $(`body, html`);
 
+    /** Initialize menu */
+    let _init = () => {
+        _$menu = $(`.menu`);
+        _$toggler = $(`.hamburger`);
+
+        _$toggler.on('click', e => {
+            e.preventDefault();
+            if (_active) {
+                _$page.css(`overflow`, `visible`);
+                _$menu.removeClass(`menu_state_active`);
+                _$toggler.removeClass(`hamburger_state_cross`);
+                _active = false;
+            } else {
+                _$page.css(`overflow`, `hidden`);
+                _$menu.addClass(`menu_state_active`);
+                _$toggler.addClass(`hamburger_state_cross`);
+                _active = true;
+            }
+        });
+    }
+
     return {
-        init: () => {
-            _$menu = $(`.menu`);
-            _$toggler = $(`.hamburger`);
-
-            _$toggler.on('click', e => {
-                e.preventDefault();
-                if (_active) {
-                    _$page.css(`overflow`, `visible`);
-                    _$menu.removeClass(`menu_state_active`);
-                    _$toggler.removeClass(`hamburger_state_cross`);
-                    _active = false;
-                } else {
-                    _$page.css(`overflow`, `hidden`);
-                    _$menu.addClass(`menu_state_active`);
-                    _$toggler.addClass(`hamburger_state_cross`);
-                    _active = true;
-                }
-            });
-        }
+        init: _init
     };
-
 })();
