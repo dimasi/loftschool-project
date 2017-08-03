643eb7d565ad2e93e930fa21a7fa405db7dbfb14
Dmitri Sivachenko <d.sivachenko@hotmail.com>
2017-08-03 16:55:00 +0300

    Revert "Admin panel"
    This reverts commit d8cee61bb1b419ba4c53ba431ddc42d5a7fc0d84.

--- source/pages/admin/admin.js
+++ source/pages/admin/admin.js
@@ -2,19 +2,9 @@ import 'reset-css/reset.css';
 import 'font-awesome/css/font-awesome.css';
 import './admin.scss';
 
-require(`Root/node_modules/modernizr/bin/modernizr`);
-
 const preloader = require('Modules/preloader');
-const tabs = require('Modules/tabs');
 
 $(() => {
     // preloader
     preloader.init($(`.preloader`));
-
-    // tabs
-    tabs.init({
-        navLinkSelector: `.tabs__nav .nav__link`,
-        tabSelector: `.tabs__tab`,
-        navLinkActiveClass: `nav__link_state_active`
-    });
 });
